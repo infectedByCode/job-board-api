536fbeffac78bf2ca06660d67b4d413cdb76f20b
Matt Daley <mdaleyhk@gmail.com>
2020-09-21 20:33:37 +0100

    add health checker route

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// health checker
+app.all('/', (req, res, next) => {
+  res.json({
+    status: 200,
+    msg: 'OK',
+  });
+});
+
 app.listen(port, () => {
   console.log('server started');
 });
