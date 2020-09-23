6e3ed34f580a79f8ae3e4bbf4539616167dfa63e
Matt Daley <mdaleyhk@gmail.com>
2020-09-23 19:54:25 +0100

    add getJobs from db

--- controllers/jobs-controller.js
+++ controllers/jobs-controller.js
@@ -1,6 +1,9 @@
-const { selectJobs } = require("../models/job-models");
+const { selectJobs } = require('../models/job-models');
 
 exports.getJobs = async (req, res, next) => {
   const data = await selectJobs();
+  if (data instanceof Error) {
+    return next(data);
+  }
   res.json({ jobs: data });
 };
--- models/job-models.js
+++ models/job-models.js
@@ -1,6 +1,13 @@
+const db = require('../db/connection');
+
 exports.selectJobs = () => {
-  return [
-    { jobId: 1, jobTitle: "title" },
-    { jobId: 2, jobTitle: "title" },
-  ];
+  return db
+    .promise()
+    .query('SELECT * FROM job')
+    .then(([rows]) => {
+      return rows;
+    })
+    .catch((err) => {
+      return err;
+    });
 };
