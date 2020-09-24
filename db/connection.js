9feaf19b9e08d3c1ecc76f8d7f6214a08bd2e1de
Matt Daley <mdaleyhk@gmail.com>
2020-09-24 20:08:35 +0100

    update config settings for db

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database: process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
