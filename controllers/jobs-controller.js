962edcb941721f3931efd71aea0818ec92595d25
Matt Daley <mdaleyhk@gmail.com>
2020-09-28 19:49:22 +0100

    rename data to result for clarity

--- controllers/jobs-controller.js
+++ controllers/jobs-controller.js
@@ -8,26 +8,26 @@ const {
 } = require('../models/job-models');
 
 exports.getJobs = async (req, res, next) => {
-  const data = await selectJobs();
-  if (data instanceof Error) {
-    return next(data);
+  const result = await selectJobs();
+  if (result instanceof Error) {
+    return next(result);
   }
   return res.status(200).json({
     status: 200,
-    jobs: data,
+    jobs: result,
   });
 };
 
 exports.postJob = async (req, res, next) => {
-  const data = await insertJob(req.body);
-  if (data instanceof Error) {
-    return next(data);
+  const result = await insertJob(req.body);
+  if (result instanceof Error) {
+    return next(result);
   }
-  if (data.affectedRows === 1) {
+  if (result.affectedRows === 1) {
     return res.status(201).json({
       status: 201,
       msg: 'job created successfully',
-      ref: data.jobId,
+      ref: result.jobId,
     });
   }
 };
@@ -35,35 +35,35 @@ exports.postJob = async (req, res, next) => {
 exports.getJobsByTerm = async (req, res, next) => {
   const { searchTerm } = req.params;
   const searchList = searchTerm.split('+');
-  const data = await selectJobsByTerm(searchList);
-  if (data instanceof Error) {
-    return next(data);
+  const result = await selectJobsByTerm(searchList);
+  if (result instanceof Error) {
+    return next(result);
   }
   return res.status(200).json({
     status: 200,
-    jobs: data,
+    jobs: result,
   });
 };
 
 exports.getJobsByCompanyId = async (req, res, next) => {
   const { companyId } = req.params;
-  const data = await selectJobsByCompanyId(companyId);
-  if (data instanceof Error) {
-    return next(data);
+  const result = await selectJobsByCompanyId(companyId);
+  if (result instanceof Error) {
+    return next(result);
   }
   return res.status(200).json({
     status: 200,
-    jobs: data,
+    jobs: result,
   });
 };
 
 exports.getJobById = async (req, res, next) => {
   const { jobId } = req.params;
-  const data = await selectJobById(jobId);
-  if (data instanceof Error) {
-    return next(data);
+  const result = await selectJobById(jobId);
+  if (result instanceof Error) {
+    return next(result);
   }
-  if (data.length === 0) {
+  if (result.length === 0) {
     return res.status(200).json({
       status: 204,
       msg: 'No content',
@@ -71,20 +71,20 @@ exports.getJobById = async (req, res, next) => {
   }
   return res.status(200).json({
     status: 200,
-    job: data[0],
+    job: result[0],
   });
 };
 
 exports.patchJobById = async (req, res, next) => {
   const { jobId } = req.params;
-  const data = await updateJobById(jobId, req.body);
-  if (data instanceof Error) {
-    return next(data);
+  const result = await updateJobById(jobId, req.body);
+  if (result instanceof Error) {
+    return next(result);
   }
-  if (data.affectedRows === 1) {
+  if (result.affectedRows === 1) {
     return res.status(200).json({
       status: 200,
-      msg: `job ${data.jobId} updated successfully`,
+      msg: `job ${result.jobId} updated successfully`,
     });
   }
 };
